8017d311843d87fd387e412897f7c240e485f603
eibarrosan1 <eibarrosan1@educacion.navarra.es>
2021-11-14 14:25:19 +0100

    eibarrosan1:modificar el index, porque se me habia olvidado el titulo del empleado por comisión

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once "clases/EmpleadoPorComision.php";
 $resultado = "<strong>Empleado:</strong> <br><br>";
 $resultado .= "<strong>Empleado Asalariado</strong><br>";
 $empleado1 = new EmpleadoPlantilla("Lola", "Etxegaray", "14-78451589-75", 755, 5);
-$resultado .= $empleado1->mostrar() . "<br><br>";
+$resultado .= $empleado1->mostrar() . "<br><br><strong>Empleado Por Comision: </strong><br>";
 $empleado2 = new EmpleadoPorComision("Eunate", "Ibarrola Santesteban", "14-4569871236-15", 8, 20, 2);
 $resultado .= $empleado2->mostrar();
 include "vistas/vista_resultado.php"
